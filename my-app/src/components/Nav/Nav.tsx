2e749db2ace46067f8f34d55dc6f153f3559a071
MonnoDev <toleikytkamil500@gmail.com>
2025-06-02 18:16:33 +0300

    admin

--- my-app/src/components/Nav/Nav.tsx
+++ my-app/src/components/Nav/Nav.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { LOGIN_ROUTE, REGISTER_ROUTE } from "../../routes/const";
+import { LOGIN_ROUTE, MAIN_ROUTE, REGISTER_ROUTE } from "../../routes/const";
 import Button from "../Button/Button";
 import Gym from "../../images/Gym_.png";
-import "./Nav.css"
+import "./Nav.css";
 
-  const Nav: React.FC = () => {
-    return (
-      <nav className="navbar">
-        <div className="logo">
-          <img src={Gym} alt="Gym" />
-        </div>
-        <div className="buttonContainer">
-          <Button><Link to={LOGIN_ROUTE}>Log in</Link></Button>
-          <Button><Link to={REGISTER_ROUTE}>Register</Link></Button>
-        </div>
-      </nav>
-    );
-  };
-  
+const Nav: React.FC = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const adminFlag = sessionStorage.getItem("admin");
+    setIsAdmin(adminFlag === "true");
+  }, []);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={Gym} alt="Gym" />
+      </div>
+      <div className="buttonContainer">
+        {isAdmin ? (
+          <>
+            <Button onClick={() => {
+              sessionStorage.removeItem("admin");
+              window.location.reload();
+            }}><Link to={MAIN_ROUTE}>Log out</Link></Button>
+          </>
+        ) : (
+          <>
+            <Button><Link to={LOGIN_ROUTE}>Log in</Link></Button>
+            <Button><Link to={REGISTER_ROUTE}>Register</Link></Button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
 export default Nav;
--- my-app/src/pages/Admin/Admin.tsx
+++ my-app/src/pages/Admin/Admin.tsx
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import './Admin.css';
+import Nav from "../../components/Nav/Nav";
 
 const Admin: React.FC = () => {
 
     return (
         <div>
-            <p>Admin Panel</p>
-            <ul>
+            <Nav/>
+            <p>Gym admin panel</p>
+            <ul className="adminPanelItems">
                     <li><Link to="/users">Manage Users</Link></li>
                     <li><Link to="/createmembership">Create Membership</Link></li>
                     <li><Link to="/createuser">Create User</Link></li>
